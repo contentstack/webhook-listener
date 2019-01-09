e27b6d9ceaccfa9e0a6485dfa9413d5fae447391
Ninad Hatkar <ninadh22@gmail.com>
2019-01-09 11:15:18 +0530

    added logger

--- example/index.js
+++ example/index.js
@@ -1,4 +1,5 @@
 const listener = require('../dist')
+const winston = require('winston')
 
 
 // NOTE: Registering method should have a 'notify' method
@@ -6,6 +7,19 @@ let notify = function (response) {
 		//console.dir(response, {showHidden: true, colors: true, depth: null})
 	}
 
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	defaultMeta: {service: 'user-service'},
+	transports: [
+		//
+		// - Write to all logs with level `info` and below to `combined.log` 
+		// - Write all logs error (and below) to `error.log`.
+		//
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'combined.log' })
+	]
+});
 
 // Register modules - the module's notify will be called on events
 listener.register(notify)
@@ -19,4 +33,4 @@ listener.start({
 			pass: "admin"
 		}
 	}	
-})
+}, logger)
