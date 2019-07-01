77003777beb6c9051d3854c4494d4b4967e70478
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-07-01 18:22:06 +0530

    module update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
 'use strict';
 
 import { debug as Debug } from 'debug';
-import { merge as _merge } from 'lodash';
+import { merge } from 'lodash';
 import { createListener } from './core';
 import { defaultConfig } from './defaults';
 import { logger as log, setLogger } from './logger';
@@ -15,6 +15,7 @@ import { logger as log, setLogger } from './logger';
 const debug = Debug('webhook:listener');
 let notify;
 let config: any = {};
+let appConfig: any = defaultConfig
 
 /**
  * Register a function that will get called when webhook is triggered.
@@ -38,38 +39,47 @@ export function register(consumer: any) {
  * @returns {Promise} Promise object represents http.Server
  */
 export function start(userConfig: any, customLogger?: any) {
-  if (customLogger) {
-    setLogger(customLogger);
-  }
   return new Promise((resolve, reject) => {
-    debug('start called with %O', userConfig);
-    validateConfig(userConfig);
-    // Override default with user config
-    if (userConfig) {
-      config = _merge({}, defaultConfig, userConfig);
-    } else {
-      log.info('Starting listener with default configs');
-      config = defaultConfig;
-    }
+    try {
+      if (customLogger) {
+        setLogger(customLogger);
+      }
+      debug('start called with %O', userConfig);
+      appConfig = merge(appConfig, userConfig)
+      validateConfig(appConfig);
 
-    if (!notify) {
-      log.error(
-        'Aborting start of webhook listener, since no function is provided to notify.',
-      );
-      return reject(
-        new Error(
-          `Aborting start of webhook listener, since no function is provided to notify.`,
-        ),
-      );
-    }
+      if (!notify) {
+        log.error(
+          'Aborting start of webhook listener, since no function is provided to notify.',
+        );
+        return reject(
+          new Error(
+            `Aborting start of webhook listener, since no function is provided to notify.`,
+          ),
+        );
+      }
 
-    debug('starting with config: ' + JSON.stringify(config));
-    const port = process.env.PORT || config.listener.port;
-    const server = createListener(config, notify).listen(port, () => {
-      log.info(`Server running at port ${port}`);
-    });
-    return resolve(server);
+      debug('starting with config: ' + JSON.stringify(appConfig));
+      const port = process.env.PORT || appConfig.listener.port;
+      const server = createListener(appConfig, notify).listen(port, () => {
+        log.info(`Server running at port ${port}`);
+      });
+      return resolve(server);
+    } catch (error) {
+      return reject(error)
+    }
   });
+}
+
+/**
+ * @public
+ * @method setConfig
+ * @description
+ * Sets listener library's configuration
+ * @param config Listener lib config
+ */
+export const setConfig = (config) => {
+  appConfig = merge(appConfig, config)
 }
 
 /**
